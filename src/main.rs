ec14e7198907cc1602474b25826e1d78645d0aa2
daa <andrey.dubravin@gmail.com>
2016-02-06 20:56:51 +0300

    Right way to load config

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,87 @@
 
+extern crate rustc_serialize;
 extern crate toml;
+extern crate ftp;
 
 use std::io::prelude::*;
 use std::fs::File;
 use std::error::Error;
 use std::process::Command;
 
+use ftp::FTPStream;
+
+#[derive(RustcDecodable)]
+struct Ftp {
+    host: String,
+    port: u16,
+    user: String,
+    pass: String
+}
+
+#[derive(RustcDecodable)]
+struct Run {
+    commands: Vec<String>,
+}
+
+#[derive(RustcDecodable)]
+struct Src {
+    path: String,
+    prefix: String
+}
+
+#[derive(RustcDecodable)]
 struct Settings {
-    commands: Vec<String>
+    run: Run,
+    ftp: Ftp,
+    src: Vec<Src>
 }
 
 const CONFIG_FILE: &'static str = "config.toml";
 
 fn main() {
+    let settings = load_settings();
 
     // TODO: process errors to email
-    match run() {
-        Err(e) => println!("Error in backup {}", e.description()),
+    match run(&settings) {
+        Err(e) => println!("Error in backup: {}", e.description()),
         Ok(_) => println!("Backup finished")
     }
     
 }
 
-fn run() -> Result<(), SimpleError> {
-    let settings = try!(load_settings());
-    try!(run_commands(&settings.commands));
+fn run(settings: &Settings) -> Result<(), SimpleError> {
+    try!(run_commands(&settings.run.commands));
+    try!(send_to_ftp(&settings));
+    Ok(())
+}
+
+fn send_to_ftp(settings: &Settings) -> Result<(), SimpleError> {
+    let mut ftp_stream = try!(FTPStream::connect(settings.ftp.host.to_owned(), settings.ftp.port));
+    try!(ftp_stream.login(&settings.ftp.user, &settings.ftp.pass));
+    ftp_stream.quit();
     Ok(())
 }
 
 fn run_commands(commands: &[String]) -> Result<(), SimpleError> {
+    println!("Execute commands");
     for command in commands {
-        println!("Execute {} ...", command);
+        println!("Execute '{}' ...", command);
         let status = try!(Command::new(command).status());
-        println!("Status {}", status);
+        println!("Status '{}'", status);
     }
     Ok(())
 }
 
-fn load_settings() -> Result<Settings, SimpleError> {
+fn load_settings() -> Settings {
     println!("Load config '{}' ...", CONFIG_FILE);
 
-    let mut f = try!(File::open(CONFIG_FILE));
+    let mut f = File::open(CONFIG_FILE).expect("Can't open config file");
 
     let mut config_str = String::new();
 
-    try!(f.read_to_string(&mut config_str));
+    f.read_to_string(&mut config_str).expect("Can't read config file");
 
-    let mut parser = toml::Parser::new(&config_str);
-    let config = try!(parser.parse().ok_or(SimpleError::Str(format!("Can't parse config file, {:?}", parser.errors))));
-
-    let commands = match config.get("run") {
-        None => {
-            println!("No run section found");
-            vec![]
-        },
-        Some(run) => {
-            match run.lookup("commands") {
-                None => {
-                    println!("No commands found");
-                    vec![]
-                },
-                Some(commands) => {
-                    let commands_slice = 
-                        try!(commands.as_slice().ok_or(SimpleError::Str("Wrong type of comands section".to_owned())));
-                    try!(commands_slice.iter()
-                         .map(|v| v.as_str().map(|s| s.to_string()).ok_or(SimpleError::Str("Command must be string".to_owned())))
-                         .collect::<Result<Vec<String>, SimpleError>>())
-                }
-            }
-        }
-    };
-
-    Ok(Settings {
-        commands: commands
-    })
+    toml::decode_str(&config_str).expect("can't decode config string")
 }
 
 
@@ -89,6 +97,11 @@ impl From<std::io::Error> for SimpleError {
     }
 }
 
+impl From<String> for SimpleError {
+    fn from(e: String) -> Self {
+        SimpleError::Str(e)
+    }
+}
 
 impl std::fmt::Display for SimpleError {
     fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {

0a710a668c05ec5d80f67d20f746b37c5ba170e2
daa <andrey.dubravin@gmail.com>
2016-02-13 11:40:04 +0300

    store file to ftp, generate ftp filename, logs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 
+#[macro_use]
+extern crate log;
+extern crate log4rs;
+
 extern crate rustc_serialize;
 extern crate toml;
 extern crate ftp;
 extern crate tempdir;
 extern crate zip;
 extern crate walkdir;
+extern crate time;
 
 use std::io::prelude::*;
 use std::fs::File;
+use std::path::{Path, PathBuf};
 use std::error::Error;
 use std::process::Command;
 
@@ -19,12 +25,17 @@ use ftp::FTPStream;
 
 use walkdir::WalkDir;
 
+use time::{strftime, now};
+
 #[derive(RustcDecodable)]
 struct Ftp {
     host: String,
     port: u16,
     user: String,
-    pass: String
+    pass: String,
+    backup_dir: String,
+    backup_file_name: String,
+    backup_suffix_format: String
 }
 
 #[derive(RustcDecodable)]
@@ -48,6 +59,8 @@ struct Settings {
 const CONFIG_FILE: &'static str = "config.toml";
 
 fn main() {
+    log4rs::init_file("log.toml", Default::default()).unwrap();
+
     let settings = load_settings();
 
     // TODO: process errors to email
@@ -62,13 +75,14 @@ fn run(settings: &Settings) -> Result<(), String> {
     try!(run_commands(&settings.run.commands));
 
     let temp_dir = try!(TempDir::new("backup-tool").map_err(|e|e.to_string()));
-    try!(create_archive(&temp_dir, &settings.src));
-    try!(send_to_ftp(&settings));
+    let archive = try!(create_archive(&temp_dir, &settings.src));
+    try!(send_to_ftp(&archive, &settings));
     Ok(())
 }
 
-fn create_archive(temp_dir: &TempDir, src_list: &Vec<Src>) -> Result<(), String> {
-    let file = try!(File::create(&temp_dir.path().join("backup.zip")).map_err(|e|e.to_string()));
+fn create_archive(temp_dir: &TempDir, src_list: &Vec<Src>) -> Result<PathBuf, String> {
+    let archive_path = temp_dir.path().join("backup.zip");
+    let file = try!(File::create(&archive_path).map_err(|e|e.to_string()));
 
     let mut zip = ZipWriter::new(file);
 
@@ -77,14 +91,14 @@ fn create_archive(temp_dir: &TempDir, src_list: &Vec<Src>) -> Result<(), String>
     }
 
     try!(zip.finish().map_err(|e|e.to_string()));
-    Ok(())
+    Ok(archive_path)
 }
 
 fn write_dir(zip: &mut ZipWriter<File>, src: &Src) -> Result<(), String> {
     for entry in WalkDir::new(&src.path) {
         let dir_entry = try!(entry.map_err(|e|e.to_string()));
         let path = dir_entry.path();
-        let zip_path = path.join(&src.prefix);
+        let zip_path = Path::new(&src.prefix).join(&path);
         
         try!(zip.start_file(zip_path.to_str().unwrap(), zip::CompressionMethod::Stored).map_err(|e|e.to_string()));
         if path.is_file() {
@@ -95,26 +109,44 @@ fn write_dir(zip: &mut ZipWriter<File>, src: &Src) -> Result<(), String> {
     Ok(())
 }
 
-fn send_to_ftp(settings: &Settings) -> Result<(), String> {
+fn send_to_ftp(archive: &Path, settings: &Settings) -> Result<(), String> {
     let mut ftp_stream = try!(FTPStream::connect(settings.ftp.host.to_owned(), settings.ftp.port).map_err(|e|e.to_string()));
     try!(ftp_stream.login(&settings.ftp.user, &settings.ftp.pass));
+
+    try!(ftp_stream.change_dir(&settings.ftp.backup_dir));
+    let time_str = try!(strftime(&settings.ftp.backup_suffix_format, &now()).map_err(|e| e.to_string()));
+    let target_file_format = format!("{}-{}", settings.ftp.backup_file_name, time_str);
+
+    let mut target_file = format!("{}.zip", target_file_format);
+    for i in 1..100 {
+        if let Err(_) = ftp_stream.retr(&target_file) {
+            break;
+        }
+
+        if i >= 99 {
+            return Err("To match backup files exists".to_owned());
+        }
+        target_file = format!("{}-{}.zip", target_file_format, i);
+    }
+    let mut src_file = try!(File::open(archive).map_err(|e| e.to_string()));
+    try!(ftp_stream.stor(&target_file, &mut src_file));
 
     try!(ftp_stream.quit().map_err(|e|e.to_string()));
     Ok(())
 }
 
 fn run_commands(commands: &[String]) -> Result<(), String> {
-    println!("Execute commands");
+    info!("Execute commands");
     for command in commands {
-        println!("Execute '{}' ...", command);
+        info!("Execute '{}' ...", command);
         let status = try!(Command::new(command).status().map_err(|e|e.to_string()));
-        println!("Status '{}'", status);
+        info!("Status '{}'", status);
     }
     Ok(())
 }
 
 fn load_settings() -> Settings {
-    println!("Load config '{}' ...", CONFIG_FILE);
+    info!("Load config '{}' ...", CONFIG_FILE);
 
     let mut f = File::open(CONFIG_FILE).expect("Can't open config file");
 

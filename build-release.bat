fc0478512aa9f7fdb5fa03a366ce1cfde6596a0e
daa84 <daa84@inbox.ru>
2016-02-20 16:55:54 +0300

    Fix release build

--- build-release.bat
+++ build-release.bat
@@ -1,6 +1,6 @@
-rem SET OPENSSL_LIB_DIR=c:/OpenSSL-Win64
+SET OPENSSL_LIB_DIR=c:/OpenSSL-Win64
 SET OPENSSL_INCLUDE_DIR=c:/OpenSSL-Win64/include
-SET OPENSSL_STATIC=yes
-SET OPENSSL_LIB_DIR=C:\OpenSSL-Win64\lib\VC\static
-cargo build --release -- -C link_args="-Wl,--subsystem,windows"
+rem SET OPENSSL_STATIC=yes
+rem -- -C link_args="-Wl,--subsystem,windows"
+cargo build --release
 pause
